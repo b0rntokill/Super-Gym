3c93d489c3edb6a3dd55817793d5289b2017d848
b0rntokill <zappbranigan39@gmail.com>
2020-02-19 15:32:34 +0200

    Исправление скриптов на es5, ошибка нерабочего слайдера в ie11 по прежнему на vtcnt

---
+++ build/js/main.js
@@ -0,0 +1,124 @@
+"use strict";
+
+var getRebootProtection = function getRebootProtection(inputFields) {
+  if (window.localStorage) {
+    (function () {
+      var elements = document.querySelectorAll(inputFields);
+      var storage = {};
+      var checkStorage = JSON.parse(localStorage.getItem("inputMemory"));
+      checkStorage !== null ? storage = checkStorage : console.log("\u041A\u0430\u0437\u043D\u0430 \u043F\u0443\u0441\u0442\u0430, \u043C\u0438\u043B\u043E\u0440\u0434");
+
+      var _loop = function _loop(i) {
+        var name = elements[i].getAttribute("name");
+        elements[i].value = storage[name] || "";
+
+        elements[i].onkeyup = function () {
+          storage[name] = elements[i].value;
+          localStorage.setItem("inputMemory", JSON.stringify(storage));
+        };
+      };
+
+      for (var i = 0; i < elements.length; i++) {
+        _loop(i);
+      }
+    })();
+  }
+};
+
+var setPhoneMask = function setPhoneMask(phoneMask, fields) {
+  var maskOptions = {
+    mask: phoneMask
+  };
+  var elements = document.querySelectorAll(fields);
+
+  for (var i = 0; i < elements.length; i++) {
+    var mask = IMask(elements[i], maskOptions);
+  }
+};
+
+getRebootProtection("input");
+setPhoneMask("+{7}(000)000-00-00", "input[type=\"tel\"]");
+var trainersSwiper = new Swiper(".trainers__slider", {
+  loop: true,
+  navigation: {
+    nextEl: ".trainers__slider-button-next",
+    prevEl: ".trainers__slider-button-prev"
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 0
+    },
+    600: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 30
+    },
+    768: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 30
+    },
+    800: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 60
+    },
+    830: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 70
+    },
+    860: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 80
+    },
+    900: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 95
+    },
+    950: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 5
+    },
+    1050: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 10
+    },
+    1100: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 25
+    },
+    1200: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+      spaceBetween: 25
+    },
+    1250: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+      spaceBetween: 30
+    },
+    1300: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+      spaceBetween: 40
+    }
+  }
+});
+var reviewsSwiper = new Swiper(".reviews__slider", {
+  loop: true,
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  navigation: {
+    nextEl: ".reviews__slider-button-next",
+    prevEl: ".reviews__slider-button-prev"
+  }
+});
+//# sourceMappingURL=main.js.map
--- source/js/script.js
+++ source/js/script.js
@@ -4,25 +4,109 @@ const getRebootProtection = (inputFields) => {
     let storage = {};
     let checkStorage = JSON.parse(localStorage.getItem(`inputMemory`));
     checkStorage !== null ? storage = checkStorage : console.log(`Казна пуста, милорд`);
-    Array.from(elements).forEach((element) => {
-      let name = element.getAttribute(`name`);
-      element.value = storage[name] || ``;
-      element.onkeyup = function () {
-        storage[name] = element.value;
+    for (let i = 0; i < elements.length; i++) {
+      let name = elements[i].getAttribute(`name`);
+      elements[i].value = storage[name] || ``;
+      elements[i].onkeyup = function () {
+        storage[name] = elements[i].value;
         localStorage.setItem(`inputMemory`, JSON.stringify(storage));
       };
-    });
+    }
   }
 };
 
 const setPhoneMask = (phoneMask, fields) => {
   const maskOptions = {mask: phoneMask};
   let elements = document.querySelectorAll(fields);
-  Array.from(elements).forEach((element) => {
-    let mask = IMask(element, maskOptions);
-  });
+  for (let i = 0; i < elements.length; i++) {
+    let mask = IMask(elements[i], maskOptions);
+  }
 };
 
 getRebootProtection(`input`);
 setPhoneMask(`+{7}(000)000-00-00`, `input[type="tel"]`);
 
+const trainersSwiper = new Swiper(`.trainers__slider`, {
+  loop: true,
+  navigation: {
+    nextEl: `.trainers__slider-button-next`,
+    prevEl: `.trainers__slider-button-prev`,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 0,
+    },
+    600: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 30,
+    },
+    768: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 30,
+    },
+    800: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 60,
+    },
+    830: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 70,
+    },
+    860: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 80,
+    },
+    900: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 95,
+    },
+    950: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 5,
+    },
+    1050: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 10,
+    },
+    1100: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 25,
+    },
+    1200: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+      spaceBetween: 25,
+    },
+    1250: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+      spaceBetween: 30,
+    },
+    1300: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+      spaceBetween: 40,
+    }
+  },
+});
+
+const reviewsSwiper = new Swiper(`.reviews__slider`, {
+  loop: true,
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  navigation: {
+    nextEl: `.reviews__slider-button-next`,
+    prevEl: `.reviews__slider-button-prev`,
+  },
+});
