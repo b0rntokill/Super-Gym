5e169a089c35357324c1cea0fafc7e7700e4c1a0
b0rntokill <zappbranigan39@gmail.com>
2020-02-24 13:38:17 +0200

    Исправляет все выявленные недостатки/ошибки

--- build/js/main.js
+++ build/js/main.js
@@ -23,8 +23,44 @@ const setPhoneMask = (phoneMask, fields) => {
   }
 };
 
+const setSmoothScrollAnchors = (anchors) => {
+  const links = document.querySelectorAll(anchors);
+  // коэффициент скорости скроллинга
+  const speed = 0.2;
+
+  for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener(`click`, function (evt) {
+      evt.preventDefault();
+      // расстояние от верха документа до верхней части окна браузера
+      const windowHeight = window.pageYOffset;
+      // id элемента, к которому нужно перейти
+      const anchorId = links[i].getAttribute(`href`);
+      // отступ от верха окна браузера до верха id
+      const distanceToElement = document.querySelector(anchorId).getBoundingClientRect().top;
+      let start = null;
+
+      let step = (time) => {
+        if (start === null) {
+          start = time;
+        }
+
+        const progress = time - start;
+        const shift = (distanceToElement < 0 ? Math.max(windowHeight - progress / speed, windowHeight + distanceToElement) : Math.min(windowHeight + progress / speed, windowHeight + distanceToElement));
+        window.scrollTo(0, shift);
+
+        if (shift !== windowHeight + distanceToElement) {
+          requestAnimationFrame(step);
+        }
+      };
+
+      requestAnimationFrame(step);
+    });
+  }
+};
+
 getRebootProtection(`input`);
 setPhoneMask(`+{7}(000)000-00-00`, `input[type="tel"]`);
+setSmoothScrollAnchors(`[href^="#"]`);
 
 const trainersSwiper = new Swiper(`.trainers__slider`, {
   loop: true,
@@ -101,6 +137,7 @@ const reviewsSwiper = new Swiper(`.reviews__slider`, {
   loop: true,
   slidesPerView: 1,
   slidesPerGroup: 1,
+  // autoHeight: true,
   nextButton: `.reviews__slider-button-next`,
-  prevButton: `.reviews__slider-button-prev`
+  prevButton: `.reviews__slider-button-prev`,
 });
